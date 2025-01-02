8fd44b0332b669a3aea8f83373fa4ce531f4f50f
Sabir <badr541000@gmail.com>
2025-01-02 18:11:25 +0100

    [Adding] Creating the API end-points for the Product and Category

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'create']);
+
+Route::get('/categories', [CategoryController::class, 'create']);
+Route::post('/categories', [CategoryController::class, 'create']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
